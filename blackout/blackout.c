3116fcb7976df64efaaa660de13de00647790901
BrennaManning <brenna.manning@students.olin.edu>
2017-02-27 20:06:10 -0500

    code restructure - still needs testing

--- blackout/blackout.c
+++ blackout/blackout.c
@@ -5,77 +5,59 @@
 #include "ui.h"
 #include "timer.h"
 #include "oc.h"
+#include "spi.h"
 
 
-typedef void (*STATE_HANDLER_T)(void);
+_PIN *bP1; 
+_PIN *bP2; 
+_PIN *ledP1; 
+_PIN *ledP2; 
 
-void on1(void); // button1 on
-void off1(void); // button1 off
-void on2(void); // button2 on 
-void off2(void); // button2 off
-int button1 = 0;
-int button2 = 0;
-int lastbutton1 = 0;
-int lastbutton2 = 0;
+int panel;
 
-// int b1Addr = &D[4];
+int readButtons(int panel);
+void writeLights(int panel, int leds);
 
-STATE_HANDLER_T state1, last_state1;
-STATE_HANDLER_T state2, last_state2;
+int leds_p1; 
+int buttons_p1;
+int prev_buttons_p1;
+int diff_buttons_p1; 
 
-void on1(void) {
-    if (state1 != last_state1) {
-        pin_set(&D[4]);
-        last_state1 = state1;
+int leds_p2;
+int buttons_p2;
+int prev_buttons_p2;
+int diff_buttons_p2;
+
+int prev_leds_p1;
+int prev_leds_p2;
+
+
+int readButtons(panel){
+    if (panel == 1){
+        return pin_read(bP1);
+        // SPI Stuff
+        // Clock
+        
     }
-
-    else if (button1 == 1 && button1 != lastbutton1) { 
-        led_off(&led1);
-        last_state1 = state1;
-        state1 = off1;
-    }
-}
-
-void off1(void) {
-    if (state1 != last_state1) {
-        // led_on(&led3);
-        pin_clear(&D[4]);
-        last_state1 = state1;
-    }
-    
-    else if (button1 == 1  && button1 != lastbutton1) {
-        led_on(&led1);
-        last_state1 = state1;
-        state1 = on1;
+    else if (panel == 2){
+        return pin_read(bP2);
     }
 }
 
-void on2(void) {
-    if (state2 != last_state2) {
-        pin_set(&D[5]);
-        last_state2 = state2;
+void writeLights(panel, leds){
+    if (panel == 1){
+        pin_write(ledP1, leds);
+        // SPI CLOCK STUFF
+        led_toggle(&led1);
+        
     }
-
-    else if (button2 == 1 && button2 != lastbutton2) { 
-        led_off(&led3);
-        last_state2 = state2;
-        state2 = off2;
-    }
-}
-
-void off2(void) {
-    if (state2 != last_state2) {
-        // led_on(&led3);
-        pin_clear(&D[5]);
-        last_state2 = state2;
-    }
-    
-    else if (button2 == 1  && button2 != lastbutton2) {
-        led_on(&led3);
-        last_state2 = state2;
-        state2 = on2;
+    else if (panel == 2){
+        pin_write(ledP2, leds);
+        // SPI CLOCK STUFF
+        led_toggle(&led2);
     }
 }
+
 
 
 int16_t main(void) {
@@ -84,40 +66,61 @@ int16_t main(void) {
     init_pin();
     init_ui();
     init_timer();
-    //init_oc();
-    pin_digitalIn(&D[0]);
-    pin_digitalIn(&D[1]);
-    pin_digitalOut(&D[4]);
-    pin_digitalOut(&D[5]);
-    //oc_pwm(&oc1, &D[13], NULL, 10e3, 0x8000);
+
+    bP1 = &D[0];
+    bP2 = &D[1];
+    ledP1 = &D[2];
+    ledP2 = &D[3];
+
+    pin_digitalIn(bP1);
+    pin_digitalIn(bP2);
+    pin_digitalOut(ledP1);
+    pin_digitalOut(ledP2);
+
+    leds_p1 = 0x0000; 
+    buttons_p1 = 0x0000;
+    prev_buttons_p1 = 0x0000;
+    diff_buttons_p1 = 0x0000; 
+
+    leds_p2 = 0x0000;
+    buttons_p2 = 0x0000;
+    prev_buttons_p2 = 0x0000;
+    diff_buttons_p2 = 0x0000;
+
+
     timer_setPeriod(&timer2, 0.15);
     timer_start(&timer2);
-    state1 = off1;
-    state2 = off2;
-    last_state1 = (STATE_HANDLER_T)NULL;
-    last_state2 = (STATE_HANDLER_T)NULL;
+   
+
 
     while (1) {
         if (timer_flag(&timer2)) {
             timer_lower(&timer2);
-            lastbutton1 = button1;
-            lastbutton2 = button2;
-            button1 = pin_read(&D[0]);
-            button2 = pin_read(&D[1]);
-            state1();
-            state2();
+            prev_buttons_p1 = buttons_p1;
+            buttons_p1 = readButtons(1);
+            diff_buttons_p1 = buttons_p1 ^ prev_buttons_p1;
+            // if diff_buttons_p1 == 1
+
+            prev_buttons_p2 = buttons_p2;
+            buttons_p2 = readButtons(2);
+            diff_buttons_p2 = buttons_p2 ^ prev_buttons_p2;
+
+            prev_leds_p1 = leds_p1;
+            prev_leds_p2 = leds_p2;
+            leds_p1 = leds_p1 ^ diff_buttons_p2;
+            leds_p2 = leds_p2 ^ diff_buttons_p1;
+            if (leds_p1 != prev_leds_p1){
+
+                writeLights(1, leds_p1);
+
+            }
+            if (leds_p2 != prev_leds_p2){
+
+                writeLights(2, leds_p2);
+
+            }
+
         }
-    //     button1 = pin_read(&D[0]);
-    //     if (button1 == 1){
-    //         led_on(&led2);
-    //         pin_set(&D[4]);
-    //     }
-    
-    //     else{
-    //         led_off(&led2);
-    //         pin_clear(&D[4]);
-    //     }
-    // }
+
     }
 }
-
