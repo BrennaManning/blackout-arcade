3e35dc75c3850c2c14c8ae80a1920f698ff0a56a
BrennaManning <brenna.manning@students.olin.edu>
2017-03-26 18:52:51 -0400

    removed usb code, some testing with led

--- shiftregtest/shiftregtest.c
+++ shiftregtest/shiftregtest.c
@@ -3,21 +3,12 @@
 #include "config.h"
 #include "common.h"
 #include "ui.h"
-#include "usb.h"
+//#include "usb.h"
 #include "pin.h"
 #include "spi.h"
-/*
-#define TOGGLE_LED1         1
-#define TOGGLE_LED2         2
-#define READ_SW1            3
-#define SHIFTREG_WRITE_REG       4
-#define SHIFTREG_READ_REG        5
-#define SHIFTREG_WRITE_REGS      6
-#define SHIFTREG_READ_REGS       7
-#define TOGGLE_LED3         8 
-#define READ_SW2            9
-#define READ_SW3            10
-*/
+#include "timer.h"
+#include "oc.h"
+
 
 _PIN *SCK, *MISO, *MOSI, *CS;
 
@@ -42,6 +33,7 @@ _PIN *SCK, *MISO, *MOSI, *CS;
 
 void shiftreg_writeReg(uint8_t address, uint8_t value) {
     if (address<=0x7E) {
+        led_on(&led3);
         pin_clear(CS);
         spi_transfer(&spi1, address);
         spi_transfer(&spi1, value);
@@ -75,28 +67,26 @@ int16_t main(void) {
     MOSI = &D[1];
     MISO = &D[0];
 
-    /*    pin_init(SCK, (uint16_t *)&PORTB, (uint16_t *)&TRISB, 
-             (uint16_t *)&ANSB, 9, 9, 8, 9, (uint16_t *)&RPOR4);
-    pin_init(MISO, (uint16_t *)&PORTB, (uint16_t *)&TRISB, 
-             (uint16_t *)&ANSB, 14, 14, 0, 14, (uint16_t *)&RPOR7);
-    pin_init(MOSI, (uint16_t *)&PORTB, (uint16_t *)&TRISB, 
-             (uint16_t *)&ANSB, 8, 8, 0, 8, (uint16_t *)&RPOR4);
+    // pin_init(SCK, (uint16_t *)&PORTB, (uint16_t *)&TRISB, 
+    //          (uint16_t *)&ANSB, 9, 9, 8, 9, (uint16_t *)&RPOR4);
+    // pin_init(MISO, (uint16_t *)&PORTB, (uint16_t *)&TRISB, 
+    //          (uint16_t *)&ANSB, 14, 14, 0, 14, (uint16_t *)&RPOR7);
+    // pin_init(MOSI, (uint16_t *)&PORTB, (uint16_t *)&TRISB, 
+    //          (uint16_t *)&ANSB, 8, 8, 0, 8, (uint16_t *)&RPOR4);
 
-    pin_init(CS, (uint16_t *)&PORTB, (uint16_t *)&TRISB, 
-             (uint16_t *)NULL, 13, -1, 0, -1, (uint16_t *)NULL);
-    */
+    // pin_init(CS, (uint16_t *)&PORTB, (uint16_t *)&TRISB, 
+    //          (uint16_t *)NULL, 13, -1, 0, -1, (uint16_t *)NULL);
+
 
     pin_digitalOut(CS);
     pin_set(CS);
+    
+    while(1){
 
-    spi_open(&spi1, MISO, MOSI, SCK, 1e6, 1);
-    shiftreg_writeReg(0x13, 0x8000);
+        spi_open(&spi1, MISO, MOSI, SCK, 1e6, 1);
+        shiftreg_writeReg(0x0A, 0); // IOCON BANK TO 0
+        shiftreg_writeReg(0x15, 0b11111111); // OLAT
+        shiftreg_writeReg(0x13, 0b11111111); // GPIOB
+    }
 
-    // InitUSB();                              // initialize the USB registers and serial interface engine
-    // while (USB_USWSTAT!=CONFIG_STATE) {     // while the peripheral is not configured...
-    //     ServiceUSB();                       // ...service USB requests
-    // }
-    // while (1) {
-    //     ServiceUSB();                       // service any pending USB requests
-    // }
 }
