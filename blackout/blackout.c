924c930e277d4ebd4c345527d7a6a0f67816978c
Abigail Rodrigues <apr3010@gmail.com>
2017-03-28 23:45:50 -0400

    one panel working

--- blackout/blackout.c
+++ blackout/blackout.c
@@ -11,10 +11,18 @@ _PIN *bP1;
 _PIN *bP2; 
 _PIN *bP1_2;
 _PIN *bP2_2;
+_PIN *bP1_3;
+_PIN *bP2_3;
+_PIN *bP1_4;
+_PIN *bP2_4;
 _PIN *ledP1; 
 _PIN *ledP2;
 _PIN *ledP1_2; 
-_PIN *ledP2_2; 
+_PIN *ledP2_2;
+_PIN *ledP1_3; 
+_PIN *ledP2_3; 
+_PIN *ledP1_4; 
+_PIN *ledP2_4;  
 
 
 int panel;
@@ -36,6 +44,18 @@ int prev_buttons_p1_2;
 int diff_buttons_p1_2; 
 int diff_rising_buttons_p1_2;
 
+int leds_p1_3; 
+int buttons_p1_3;
+int prev_buttons_p1_3;
+int diff_buttons_p1_3; 
+int diff_rising_buttons_p1_3;
+
+int leds_p1_4; 
+int buttons_p1_4;
+int prev_buttons_p1_4;
+int diff_buttons_p1_4; 
+int diff_rising_buttons_p1_4;
+
 int leds_p2;
 int buttons_p2;
 int prev_buttons_p2;
@@ -48,11 +68,29 @@ int prev_buttons_p2_2;
 int diff_buttons_p2_2;
 int diff_rising_buttons_p2_2;
 
+int leds_p2_3;
+int buttons_p2_3;
+int prev_buttons_p2_3;
+int diff_buttons_p2_3;
+int diff_rising_buttons_p2_3;
+
+int leds_p2_4;
+int buttons_p2_4;
+int prev_buttons_p2_4;
+int diff_buttons_p2_4;
+int diff_rising_buttons_p2_4;
+
 int prev_leds_p1;
 int prev_leds_p2;
 
 int prev_leds_p1_2;
 int prev_leds_p2_2;
+
+int prev_leds_p1_3;
+int prev_leds_p2_3;
+
+int prev_leds_p1_4;
+int prev_leds_p2_4;
 
 int readButtons(panel, buttonNum){
     if (panel == 1){
@@ -64,6 +102,14 @@ int readButtons(panel, buttonNum){
         else if (buttonNum == 2){
             return pin_read(bP1_2);
         }
+
+        else if (buttonNum == 3){
+            return pin_read(bP1_3);
+        }
+
+        else if (buttonNum == 4){
+            return pin_read(bP1_4);
+        }
         
     }
     else if (panel == 2){
@@ -73,6 +119,14 @@ int readButtons(panel, buttonNum){
         
         else if (buttonNum == 2){
             return pin_read(bP2_2);
+        }
+
+        else if (buttonNum == 3){
+            return pin_read(bP2_3);
+        }
+
+        else if (buttonNum == 4){
+            return pin_read(bP2_4);
         }
     }
 }
@@ -85,6 +139,12 @@ void writeLights(panel, leds, lightNum){
         else if (lightNum == 2){   
             pin_write(ledP1_2, leds);
         }
+        else if (lightNum == 3){   
+            pin_write(ledP1_3, leds);
+        }
+        else if (lightNum == 4){   
+            pin_write(ledP1_4, leds);
+        }
         
     }
 
@@ -96,6 +156,12 @@ void writeLights(panel, leds, lightNum){
         else if (lightNum == 2){
             pin_write(ledP2_2, leds);
         }
+        else if (lightNum == 3){
+            pin_write(ledP2_3, leds);
+        }
+        else if (lightNum == 4){
+            pin_write(ledP2_4, leds);
+        }
     }
 }
 
@@ -106,14 +172,23 @@ int16_t main(void) {
     init_ui();
     init_timer();
 
-    bP1 = &D[0];
-    bP1_2 = &D[1];
-    bP2 = &D[2];
-    bP2_2 = &D[3];
-    ledP1 = &D[4];
-    ledP1_2 = &D[5];
-    ledP2 = &D[6];
-    ledP2_2 = &D[7];
+    bP1 = &D[8];
+    bP1_2 = &D[9];
+    bP1_3 = &D[10];
+    bP1_4 = &D[11];
+    ledP1 = &A[0];
+    ledP1_2 = &A[1];
+    ledP1_3 = &A[2];
+    ledP1_4 = &A[3];
+
+    bP2 = &D[0];
+    bP2_2 = &D[1];
+    bP2_3 = &D[2];
+    bP2_4 = &D[3];
+    ledP2 = &D[4];
+    ledP2_2 = &D[5];
+    ledP2_3 = &D[6];
+    ledP2_4 = &D[7];
 
     pin_digitalIn(bP1);
     pin_digitalIn(bP2);
@@ -121,11 +196,23 @@ int16_t main(void) {
     pin_digitalIn(bP1_2);
     pin_digitalIn(bP2_2);
 
+    pin_digitalIn(bP1_3);
+    pin_digitalIn(bP2_3);
+
+    pin_digitalIn(bP1_4);
+    pin_digitalIn(bP2_4);
+
     pin_digitalOut(ledP1);
     pin_digitalOut(ledP2);
 
     pin_digitalOut(ledP1_2);
     pin_digitalOut(ledP2_2);
+
+    pin_digitalOut(ledP1_3);
+    pin_digitalOut(ledP2_3);
+
+    pin_digitalOut(ledP1_4);
+    pin_digitalOut(ledP2_4);
 
     leds_p1 = 0x0000; 
     buttons_p1 = 0x0000;
@@ -139,6 +226,18 @@ int16_t main(void) {
     diff_buttons_p1_2 = 0x0000; 
     diff_rising_buttons_p1_2 = 0x0000;
 
+    leds_p1_3 = 0x0000; 
+    buttons_p1_3 = 0x0000;
+    prev_buttons_p1_3 = 0x0000;
+    diff_buttons_p1_3 = 0x0000; 
+    diff_rising_buttons_p1_3 = 0x0000;
+
+    leds_p1_4 = 0x0000; 
+    buttons_p1_4 = 0x0000;
+    prev_buttons_p1_4 = 0x0000;
+    diff_buttons_p1_4 = 0x0000; 
+    diff_rising_buttons_p1_4 = 0x0000;
+
     leds_p2 = 0x0000;
     buttons_p2 = 0x0000;
     prev_buttons_p2 = 0x0000;
@@ -151,6 +250,17 @@ int16_t main(void) {
     diff_buttons_p2_2 = 0x0000;
     diff_rising_buttons_p2_2 = 0x0000;
 
+    leds_p2_3 = 0x0000;
+    buttons_p2_3 = 0x0000;
+    prev_buttons_p2_3 = 0x0000;
+    diff_buttons_p2_3 = 0x0000;
+    diff_rising_buttons_p2_3 = 0x0000;
+
+    leds_p2_4 = 0x0000;
+    buttons_p2_4 = 0x0000;
+    prev_buttons_p2_4 = 0x0000;
+    diff_buttons_p2_4 = 0x0000;
+    diff_rising_buttons_p2_4 = 0x0000;
 
     timer_setPeriod(&timer2, 0.15);
     timer_start(&timer2);
@@ -169,6 +279,16 @@ int16_t main(void) {
             diff_buttons_p1_2 = buttons_p1_2 ^ prev_buttons_p1_2;
             diff_rising_buttons_p1_2 = diff_buttons_p1_2 & buttons_p1_2;
 
+            prev_buttons_p1_3 = buttons_p1_3;
+            buttons_p1_3 = readButtons(1, 3);
+            diff_buttons_p1_3 = buttons_p1_3 ^ prev_buttons_p1_3;
+            diff_rising_buttons_p1_3 = diff_buttons_p1_3 & buttons_p1_3;
+
+            prev_buttons_p1_4 = buttons_p1_4;
+            buttons_p1_4 = readButtons(1, 4);
+            diff_buttons_p1_4 = buttons_p1_4 ^ prev_buttons_p1_4;
+            diff_rising_buttons_p1_4 = diff_buttons_p1_4 & buttons_p1_4;
+
             prev_buttons_p2 = buttons_p2;
             buttons_p2 = readButtons(2, 1);
             diff_buttons_p2 = buttons_p2 ^ prev_buttons_p2;
@@ -179,18 +299,39 @@ int16_t main(void) {
             diff_buttons_p2_2 = buttons_p2_2 ^ prev_buttons_p2_2;
             diff_rising_buttons_p2_2 = diff_buttons_p2_2 & buttons_p2_2;
 
+            prev_buttons_p2_3 = buttons_p2_3;
+            buttons_p2_3 = readButtons(2, 3);
+            diff_buttons_p2_3 = buttons_p2_3 ^ prev_buttons_p2_3;
+            diff_rising_buttons_p2_3 = diff_buttons_p2_3 & buttons_p2_3;
+
+            prev_buttons_p2_4 = buttons_p2_4;
+            buttons_p2_4 = readButtons(2, 4);
+            diff_buttons_p2_4 = buttons_p2_4 ^ prev_buttons_p2_4;
+            diff_rising_buttons_p2_4 = diff_buttons_p2_4 & buttons_p2_4;
+
             prev_leds_p1 = leds_p1;
             prev_leds_p2 = leds_p2;
 
             prev_leds_p1_2 = leds_p1_2;
             prev_leds_p2_2 = leds_p2_2;
             
+            prev_leds_p1_3 = leds_p1_3;
+            prev_leds_p2_3 = leds_p2_3;
+
+            prev_leds_p1_4 = leds_p1_4;
+            prev_leds_p2_4 = leds_p2_4;
 
             leds_p1 = leds_p1 ^ diff_rising_buttons_p2;
             leds_p2 = leds_p2 ^ diff_rising_buttons_p1;
 
             leds_p1_2 = leds_p1_2 ^ diff_rising_buttons_p2_2;
             leds_p2_2 = leds_p2_2 ^ diff_rising_buttons_p1_2;
+
+            leds_p1_3 = leds_p1_3 ^ diff_rising_buttons_p2_3;
+            leds_p2_3 = leds_p2_3 ^ diff_rising_buttons_p1_3;
+
+            leds_p1_4 = leds_p1_4 ^ diff_rising_buttons_p2_4;
+            leds_p2_4 = leds_p2_4 ^ diff_rising_buttons_p1_4;
 
             if (leds_p1 != prev_leds_p1 && diff_rising_buttons_p2 != 0){
 
@@ -204,6 +345,18 @@ int16_t main(void) {
 
             }
 
+            if (leds_p1_3 != prev_leds_p1_3 && diff_rising_buttons_p2_3 != 0){
+
+                writeLights(1, leds_p1_3, 3);
+
+            }
+
+            if (leds_p1_4 != prev_leds_p1_4 && diff_rising_buttons_p2_4 != 0){
+
+                writeLights(1, leds_p1_4, 4);
+
+            }
+
             if (leds_p2 != prev_leds_p2 && diff_rising_buttons_p1 != 0){
 
                 writeLights(2, leds_p2, 1);
@@ -213,6 +366,18 @@ int16_t main(void) {
             if (leds_p2_2 != prev_leds_p2_2 && diff_rising_buttons_p1_2 != 0){
 
                 writeLights(2, leds_p2_2, 2);
+
+            }
+
+            if (leds_p2_3 != prev_leds_p2_3 && diff_rising_buttons_p1_3 != 0){
+
+                writeLights(2, leds_p2_3, 3);
+
+            }
+
+            if (leds_p2_4 != prev_leds_p2_4 && diff_rising_buttons_p1_4 != 0){
+
+                writeLights(2, leds_p2_4, 4);
 
             }
 
