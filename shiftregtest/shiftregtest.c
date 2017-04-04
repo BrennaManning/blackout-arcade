14822b933bb724c97ec4113a1a507d338bcdb0fb
BrennaManning <brenna.manning@students.olin.edu>
2017-04-03 20:51:59 -0400

    shift register reading and writing functions working

--- shiftregtest/shiftregtest.c
+++ shiftregtest/shiftregtest.c
@@ -10,29 +10,16 @@
 #include "oc.h"
 
 
-_PIN *SCK, *MISO, *MOSI, *CS, *RESET, *MISO1, *MOSI1, *CS1;
+_PIN *SCK, *MISO, *MOSI, *CS0, *RESET, *CS1;
 
-int b;
+int gpiob1;
+int olatb1;
 
-// PIN NUMBERS
-// CS &D[3]
-// SCK &D[2]
-// MOSI &D[1]
-// MISO &D[0]
+int gpiob0;
+int olatb0;
 
 
-// ADDRESSES
-// GPIOA 0x12 
-// IODIRA 0x00
-// OLATA 0x14
-
-// GPIOB 0x13
-// IODIRB 0x01
-// OLATB 0x15
-
-
-
-void shiftreg_writeReg(uint8_t address, uint8_t value) {
+void shiftreg_writeReg(uint8_t address, uint8_t value, struct _PIN * CS) {
 
     pin_clear(CS);
     // Device Opcode = 0b0100A2A1A0 A2 A1 and A0 externally set to 0
@@ -45,7 +32,7 @@ void shiftreg_writeReg(uint8_t address, uint8_t value) {
 
 }
 
-uint8_t shiftreg_readReg(uint8_t address) {
+uint8_t shiftreg_readReg(uint8_t address, struct _PIN * CS) {
     uint8_t value;
 
     pin_clear(CS);
@@ -66,7 +53,7 @@ int16_t main(void) {
     init_spi();
 
 
-    CS = &D[0];
+    CS0 = &D[0];
     SCK = &D[1];
     MOSI = &D[2];
     MISO = &D[3];
@@ -75,8 +62,6 @@ int16_t main(void) {
     RESET = &D[4];
 
     CS1 = &D[5];
-    MOSI1 = &D[6];
-    MISO1 = &D[7];
 
 
     // pin_init(SCK, (uint16_t *)&PORTB, (uint16_t *)&TRISB, 
@@ -90,29 +75,36 @@ int16_t main(void) {
     //          (uint16_t *)NULL, 13, -1, 0, -1, (uint16_t *)NULL);
 
 
-    pin_digitalOut(CS);
-    pin_set(CS);
-    spi_open(&spi1, MISO1, MOSI1, SCK, 1e6, 1);
+    pin_digitalOut(CS0);
+    pin_digitalOut(CS1);
+    pin_set(CS0);
+    pin_set(CS1);
+
+    spi_open(&spi1, MISO, MOSI, SCK, 1e6, 1);
 
     pin_clear(RESET);    
     while(1){
-        shiftreg_writeReg(0x0A, 0); // IOCON TO 0
-        shiftreg_writeReg(0x0B, 0); // IOCON TO 0
-        shiftreg_writeReg(0x00, 0); // IODIRA to 0
-        shiftreg_writeReg(0x01, 0); // IODIRB to 0
-        shiftreg_writeReg(0x14, 0b10101010); // OLATA
-        shiftreg_writeReg(0x15, 0b10101010); // OLATB
-        shiftreg_writeReg(0x12, 0b10101010); // GPIOA
-        shiftreg_writeReg(0x13, 0b10101010); // GPIOB
+        shiftreg_writeReg(0x0A, 0, CS0); // IOCON TO 0
+        shiftreg_writeReg(0x0B, 0, CS0); // IOCON TO 0
+        shiftreg_writeReg(0x00, 0, CS0); // IODIRA to 0
+        shiftreg_writeReg(0x01, 0, CS0); // IODIRB to 0
+        shiftreg_writeReg(0x14, 0b10101010, CS0); // OLATA
+        shiftreg_writeReg(0x15, 0b10101010, CS0); // olatb1
+        shiftreg_writeReg(0x12, 0b10101010, CS0); // GPIOA
+        shiftreg_writeReg(0x13, 0b10101010, CS0); // GPIOB
         
-        // b = shiftreg_readReg(0x13); // GPIOB
+        gpiob1 = shiftreg_readReg(0x13, CS1); // GPIOB
+        olatb1 = shiftreg_readReg(0x15, CS1);
 
-        // if ( b== 0b10101010){
-        //     led_on(&led1);
-        // }
-        // else if ( b== 0b11101010){
-        //     led_on(&led2);
-        // }
+        gpiob0 = shiftreg_readReg(0x13, CS0);
+        olatb0 = shiftreg_readReg(0x15, CS0);
+
+
+        if ( gpiob1 == 0b00001000){
+            led_on(&led1);
+        }
+   
+
         
     }
 
