4c2ce6ec5232447fc8274ecd965dedd82b4fb708
BrennaManning <brenna.manning@students.olin.edu>
2017-04-12 00:36:29 -0400

    All buttons controlling all lights. Need to adjust timing issue

--- blackout_shift/blackout_shift.c
+++ blackout_shift/blackout_shift.c
@@ -14,25 +14,33 @@ _PIN *SCK, *MISO, *MOSI, *RESET, *CS0, *CS1, *CS2, *CS3;
 
 // R0 Register
 int gpioa0 = 0;
-int gpiob0 = 0;
-
 int prev_gpioa0 = 0;
+int diff_gpioa0;
+int diff_rising_gpioa0;
+
+int gpiob0 = 0;
 int prev_gpiob0 = 0;
+int diff_gpiob0;
+int diff_rising_gpiob0;
 
 // W0 Register
 int olata0 = 0;
-int olatb0 = 0;
-
 int prev_olata0 = 0;
+
+int olatb0 = 0;
 int prev_olatb0 = 0;
 
 
 // R1 Register
 int gpioa1 = 0;
-int gpiob1 = 0;
-
 int prev_gpioa1 = 0;
+int diff_gpioa1;
+int diff_rising_gpioa1;
+
+int gpiob1 = 0;
 int prev_gpiob1 = 0;
+int diff_gpiob1;
+int diff_rising_gpiob1;
 
 // W1 Register
 int olata1 = 0;
@@ -119,7 +127,13 @@ int16_t main(void) {
     shiftreg_writeReg(0x00, 0, CS3); // IODIRA to 0
     shiftreg_writeReg(0x01, 0, CS3); // IODIRB to 0
 
+
+    // timer_setPeriod(&timer2, 0.15);
+    // timer_start(&timer2);
+
     while(1){
+        // if (timer_flag(&timer2)) {
+        // timer_lower(&timer2);
         
         prev_gpioa0 = gpioa0;
         prev_gpiob0 = gpiob0;
@@ -131,85 +145,36 @@ int16_t main(void) {
         gpioa1 = shiftreg_readReg(0x12, CS2); // READ GPIOA PANEL 1 REG 2
         gpiob1 = shiftreg_readReg(0x13, CS2); // READ GPIOB PANEL 1 REG 2
 
-        
+        diff_gpioa0 = gpioa0 ^ prev_gpioa0;
+        diff_gpiob0 = gpiob0 ^ prev_gpiob0;
+        diff_gpioa1 = gpioa1 ^ prev_gpioa1;
+        diff_gpiob1 = gpiob1 ^ prev_gpiob1;
 
-        // prev_buttons_p2 = buttons_p2; CHECK
-        // buttons_p2 = readButtons(2); CHECK
-        // diff_buttons_p2 = buttons_p2 ^ prev_buttons_p2;
-        // diff_rising_buttons_p2 = diff_buttons_p2 & buttons_p2;
+        diff_rising_gpioa0 = diff_gpioa0 & gpioa0;
+        diff_rising_gpiob0 = diff_gpiob0 & gpiob0;
+        diff_rising_gpioa1 = diff_gpioa1 & gpioa1;
+        diff_rising_gpiob1 = diff_gpiob1 & gpiob1;
 
+        prev_olata0 = olata0;
+        prev_olatb0 = olatb0;
+        prev_olata1 = olata1;
+        prev_olatb1 = olatb1;
 
-        shiftreg_writeReg(0x14, gpioa1, CS1); // WRITE OLATA PANEL 1 REG 3 
-        shiftreg_writeReg(0x15, gpiob1, CS1); // WRITE OLATB PANEL 1 REG 3
-        shiftreg_writeReg(0x14, gpioa0, CS3); // WRITE OLATA PANEL 1 REG 3 
-        shiftreg_writeReg(0x15, gpiob0, CS3); // WRITE OLATB PANEL 1 REG 3
+        olata0 = olata0 ^ diff_rising_gpioa1;
+        olatb0 = olatb0 ^ diff_rising_gpiob1;
+        olata1 = olata1 ^ diff_rising_gpioa0;
+        olatb1 = olatb1 ^ diff_rising_gpiob0;
+    
 
+        // shiftreg_writeReg(0x14, gpioa1, CS1); // WRITE OLATA PANEL 1 REG 3 
+        // shiftreg_writeReg(0x15, gpiob1, CS1); // WRITE OLATB PANEL 1 REG 3
+        // shiftreg_writeReg(0x14, gpioa0, CS3); // WRITE OLATA PANEL 1 REG 3 
+        // shiftreg_writeReg(0x15, gpiob0, CS3); // WRITE OLATB PANEL 1 REG 3
 
-        
-
-
-        // gpioa = shiftreg_readReg(0x12, CS0); // READ GPIOA
-        // shiftreg_writeReg(0x15, gpioa, CS0); // WRITE OLATB
-
-        // shiftreg_writeReg(0x15, 0b10101010, CS0); // olatb1
-        // shiftreg_writeReg(0x12, 0b10101010, CS0); // GPIOA
-        // shiftreg_writeReg(0x13, 0b10101010, CS0); // GPIOB
-        
-        // gpiob1 = shiftreg_readReg(0x13, CS1); // GPIOB
-        // olatb1 = shiftreg_readReg(0x15, CS1);
-
-        // gpiob0 = shiftreg_readReg(0x13, CS0);
-
-
-        // if (gpiob0 == 0b00000001){
-        //     led_on(&led1);
-        // }
-        // else{
-        //     led_off(&led1);
-        // }
-       
+        shiftreg_writeReg(0x14, olata0, CS1); // WRITE OLATA PANEL 1 REG 3 
+        shiftreg_writeReg(0x15, olatb0, CS1); // WRITE OLATB PANEL 1 REG 3
+        shiftreg_writeReg(0x14, olata1, CS3); // WRITE OLATA PANEL 1 REG 3 
+        shiftreg_writeReg(0x15, olatb1, CS3); // WRITE OLATB PANEL 1 REG 3
+        // }   
     }
-
-
-    // while (1) {
-    //     if (timer_flag(&timer2)) {
-    //         timer_lower(&timer2);
-    //         prev_buttons_p1 = buttons_p1;
-    //         buttons_p1 = readButtons(1);
-    //         diff_buttons_p1 = buttons_p1 ^ prev_buttons_p1;
-    //         diff_rising_buttons_p1 = diff_buttons_p1 & buttons_p1;
-
-
-    //         prev_buttons_p2 = buttons_p2;
-    //         buttons_p2 = readButtons(2);
-    //         diff_buttons_p2 = buttons_p2 ^ prev_buttons_p2;
-    //         diff_rising_buttons_p2 = diff_buttons_p2 & buttons_p2;
-
-    //         prev_leds_p1 = leds_p1;
-    //         prev_leds_p2 = leds_p2;
-    //         leds_p1 = leds_p1 ^ diff_rising_buttons_p2;
-    //         leds_p2 = leds_p2 ^ diff_rising_buttons_p1;
-
-    //         if (leds_p1 != prev_leds_p1 && diff_rising_buttons_p2 != 0){
-
-    //             writeLights(1, leds_p1);
-
-    //         }
-
-    //         if (leds_p2 != prev_leds_p2 && diff_rising_buttons_p1 != 0){
-
-    //             writeLights(2, leds_p2);
-
-    //         }
-
-    //     }
-
-    //     // Randomly turn on a light every 2 seconds
-    //     if (timer_flag(&timer3)) {
-    //         timer_lower(&timer3);
-    //         lightsRandom(leds_p1, leds_p2);
-    //     }
-
-    // }
-
 }
